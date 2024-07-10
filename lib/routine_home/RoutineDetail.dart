aa12f1ad58cab8c05497f28266e6f389c1a7e881
yjlee0321 <dbswjd574269@naver.com>
2024-07-11 00:51:46 +0900

    루틴페이지 간소화

---
+++ lib/routine_home/RoutineDetail.dart
@@ -0,0 +1,22 @@
+class Group { //그룹
+  final String name;
+  final DateTime creationDate;
+  final String category;
+  final int membersCount;
+  final String leader;
+  final String groupCode;
+  final String groupIntro;
+
+  Group({
+    required this.name,
+    required this.creationDate,
+    required this.category,
+    required this.membersCount,
+    required this.leader,
+    required this.groupCode,
+    required this.groupIntro,
+  });
+  
+  
+}
+
