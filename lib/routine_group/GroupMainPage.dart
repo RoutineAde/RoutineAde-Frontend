5f0b5170ba997bc03a24976e8db916ba2bab817a
yjlee0321 <129257050+yjlee0321@users.noreply.github.com>
2024-08-02 22:45:36 +0900

    인코딩문제 해결(한국어)

--- lib/routine_group/GroupMainPage.dart
+++ lib/routine_group/GroupMainPage.dart
@@ -7,7 +7,7 @@ import 'package:routine_ade/routine_group/GroupRoutinePage.dart';
 import 'package:routine_ade/routine_group/GroupType.dart';
 import 'package:routine_ade/routine_group/AddGroupPage.dart';
 
-//전체화면 어둡게 
+//전체화면 어둡게
 class DarkOverlay extends StatelessWidget {
   final Widget child;
   final bool isDark;
@@ -37,10 +37,10 @@ class DarkOverlay extends StatelessWidget {
 class GroupMainPage extends StatefulWidget {
   @override
   _GroupMainPageState createState() => _GroupMainPageState();
-  }
+}
 class _GroupMainPageState extends State<GroupMainPage>{
-    bool isExpanded = false;
-    List<Group> groups = [];
+  bool isExpanded = false;
+  List<Group> groups = [];
 
   @override
   void initState(){
@@ -51,12 +51,12 @@ class _GroupMainPageState extends State<GroupMainPage>{
     final url = Uri.parse('http://15.164.88.94:8080/groups/my');
     final response = await http.get(url, headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjA0MzIzMDYsImV4cCI6MTczNTk4NDMwNiwidXNlcklkIjoxfQ.gVbh87iupFLFR6zo6PcGAIhAiYIRfLWV_wi8e_tnqyM',
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjEwMzkzMDEsImV4cCI6MTczNjU5MTMwMSwidXNlcklkIjoyfQ.XLthojYmD3dA4TSeXv_JY7DYIjoaMRHB7OLx9-l2rvw',
     });
 
-   if (response.statusCode == 200) {
-      final decodedResponse = utf8.decode(response.bodyBytes);
-      final data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      final decodedResponse = utf8.decode(response.bodyBytes);  // UTF-8 디코딩
+      final data = jsonDecode(decodedResponse);  // 디코딩된 문자열을 JSON으로 파싱
       setState(() {
         groups = (data['groups'] as List).map((json) => Group.fromJson(json)).toList();
       });
@@ -64,6 +64,7 @@ class _GroupMainPageState extends State<GroupMainPage>{
       print("그룹 불러오기를 실패하였습니다.");
     }
   }
+
 
 //가입일자 계산
   // String calculateDaysSinceCreation(DateTime creationDate) {
@@ -74,102 +75,102 @@ class _GroupMainPageState extends State<GroupMainPage>{
 
   @override
   Widget build(BuildContext context){
-    return 
-        Scaffold( 
-      appBar: AppBar(
-        title: Text('내 그룹', 
-        style: TextStyle(
-          color: Colors.black, 
-          fontSize: 25, 
-          fontWeight: FontWeight.bold ),),
+    return
+      Scaffold(
+        appBar: AppBar(
+          title: Text('내 그룹',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.bold ),),
           centerTitle: true,
           backgroundColor: isExpanded? Colors.grey[600] : Colors.grey[200],
           automaticallyImplyLeading: false, //뒤로가기 제거
           actions: [
             Padding(padding: EdgeInsets.only(right: 16.0),
-            child: Image.asset("assets/images/bell.png",
-            width: 35,
-            height: 35,),
+              child: Image.asset("assets/images/bell.png",
+                width: 35,
+                height: 35,),
             ),
-          ], 
-      ), 
-      
-      body: DarkOverlay(
-        isDark: isExpanded, //눌렀을때만 어둡게
-        onTap: (){
-          setState(() {
-            isExpanded=false;
-          });
-        },
-        child:  Container(
-        color: Colors.grey[200], 
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: groups.length,
-                itemBuilder: (context, index) {
-                  final group = groups[index];
-                  Color textColor = group.groupCategory =="건강"? Colors.blue: Colors.orange;
-                  return  Card(
-                    margin: EdgeInsets.all(8.0),
-                    color: Colors.white,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ],
+        ),
+
+        body: DarkOverlay(
+          isDark: isExpanded, //눌렀을때만 어둡게
+          onTap: (){
+            setState(() {
+              isExpanded=false;
+            });
+          },
+          child:  Container(
+            color: Colors.grey[200],
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: groups.length,
+                    itemBuilder: (context, index) {
+                      final group = groups[index];
+                      Color textColor = group.groupCategory =="건강"? Colors.blue: Colors.orange;
+                      return  Card(
+                        margin: EdgeInsets.all(8.0),
+                        color: Colors.white,
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                group.groupTitle,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,      
-                                ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    group.groupTitle,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  if(group.isPublic)
+                                    Image.asset("assets/images/lock.png", width: 20, height: 20,), //비밀번호 방 여부
+
+                                ],
                               ),
-                              if(group.isPublic)
-                                Image.asset("assets/images/lock.png", width: 20, height: 20,), //비밀번호 방 여부
-                                
-                            ],
-                          ),
                               Text("가입 ${group.joinDate}일차"),
-                      //   ],
-                      // ),
-                          SizedBox(height: 8.0),
-                          Row(
-                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("대표 카테고리 "), 
-                              Text(group.groupCategory, style: TextStyle( color: textColor)),
-                              Expanded(child: Container()),// 간격 조절
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: Text("인원 ${group.joinMemberCount}/${group.maxMemberCount}명"),),
+                              //   ],
+                              // ),
+                              SizedBox(height: 8.0),
+                              Row(
+                                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("대표 카테고리 "),
+                                  Text(group.groupCategory, style: TextStyle( color: textColor)),
+                                  Expanded(child: Container()),// 간격 조절
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text("인원 ${group.joinMemberCount}/${group.maxMemberCount}명"),),
+                                ],
+                              ),
+                              SizedBox(height: 8.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("루틴장 ${group.createdUserNickname}"),
+                                  Text("그룹코드 ${group.groupId}"),
+                                ],
+                              ),
                             ],
                           ),
-                          SizedBox(height: 8.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("루틴장 ${group.createdUserNickname}"),
-                              Text("그룹코드 ${group.groupId}"),
-                          ],
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
-                );
-              },
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-      ),
-      ),
+        ),
 
-      //바텀 네비게이션 바
+        //바텀 네비게이션 바
         bottomNavigationBar: BottomAppBar(
           color: Colors.white,
           child: Row(
@@ -180,130 +181,130 @@ class _GroupMainPageState extends State<GroupMainPage>{
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => MyRoutinePage()),
-                );
-              },
-              child: Container(
-                width: 50,
-                height: 50,
-                child: Image.asset("assets/images/tap-bar/routine01.png"),
+                  );
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset("assets/images/tap-bar/routine01.png"),
+                ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                // 그룹 버튼 클릭 시 동작할 코드
-              },
-              child: Container(
-                width: 50,
-                height: 50,
-                child: Image.asset("assets/images/tap-bar/group02.png"),
+              GestureDetector(
+                onTap: () {
+                  // 그룹 버튼 클릭 시 동작할 코드
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset("assets/images/tap-bar/group02.png"),
+                ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                // 통계 버튼 클릭 시 동작할 코드
-              },
-              child: Container(
-                width: 50,
-                height: 50,
-                child: Image.asset("assets/images/tap-bar/statistics01.png"),
+              GestureDetector(
+                onTap: () {
+                  // 통계 버튼 클릭 시 동작할 코드
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset("assets/images/tap-bar/statistics01.png"),
+                ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                // 더보기 버튼 클릭 시 동작할 코드
-              },
-              child: Container(
-                width: 50,
-                height: 50,
-                child: Image.asset("assets/images/tap-bar/more01.png"),
+              GestureDetector(
+                onTap: () {
+                  // 더보기 버튼 클릭 시 동작할 코드
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset("assets/images/tap-bar/more01.png"),
+                ),
+              ),
+            ],
+          ),
+        ),
+        //add 버튼
+        floatingActionButton: Stack(
+          alignment: Alignment.bottomRight,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10, right: 10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (isExpanded) ...[
+                    SizedBox(height: 20),
+                    Row(mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("루틴 그룹", style:
+                        TextStyle( color: Colors.white, fontWeight: FontWeight.bold)
+                        ),
+                        SizedBox(width: 10),
+
+                        //그룹 추가 버튼
+                        FloatingActionButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(
+                              builder: (context){
+                                return  MyRoutinePage();//그룹 루틴 페이지 이동 바꿔야함
+                              },
+                            ));
+                          },
+                          backgroundColor: Color(0xffF87c3ff),
+                          child: Image.asset('assets/images/group-list.png'),
+                          shape: CircleBorder(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("그룹 추가", style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold)
+                        ),
+                        SizedBox(width: 10),
+
+                        //그룹 루틴 추가 버튼
+                        FloatingActionButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(
+                              builder: (context){
+                                return  AddGroupPage();
+                              },
+                            ));
+                          },
+                          backgroundColor: Color(0xffF1E977),
+                          child: Image.asset('assets/images/add.png'),
+                          shape: CircleBorder(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                  //add 버튼, X버튼
+                  Row(mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(""),
+                      SizedBox(width: 10),
+                      FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            isExpanded = !isExpanded;
+                          });
+                        },
+                        backgroundColor: isExpanded? Color(0xffF7C7C7C): Color(0xffF1E977),
+                        child: isExpanded
+                            ? Image.asset('assets/images/cancel.png')
+                            : Image.asset('assets/images/add.png'),
+                        shape: CircleBorder(),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
         ),
-      ),
-      //add 버튼
-    floatingActionButton: Stack(
-        alignment: Alignment.bottomRight,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10, right: 10),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (isExpanded) ...[
-                  SizedBox(height: 20),
-                  Row(mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text("루틴 그룹", style: 
-                    TextStyle( color: Colors.white, fontWeight: FontWeight.bold)
-                  ),
-                    SizedBox(width: 10),
 
-                     //그룹 추가 버튼
-                  FloatingActionButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context){
-                          return  MyRoutinePage();//그룹 루틴 페이지 이동 바꿔야함
-                        },
-                        ));
-                    },
-                    backgroundColor: Color(0xffF87c3ff),
-                  child: Image.asset('assets/images/group-list.png'),
-                  shape: CircleBorder(),
-                  ),
-                  ],
-                  ),
-                  SizedBox(height: 20),
-                  Row(mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text("그룹 추가", style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold)
-                  ),
-                     SizedBox(width: 10),
+      );
 
-                  //그룹 루틴 추가 버튼
-                  FloatingActionButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context){
-                          return  AddGroupPage();
-                        },
-                        ));
-                    },  
-                    backgroundColor: Color(0xffF1E977),
-                  child: Image.asset('assets/images/add.png'),
-                  shape: CircleBorder(),
-                  ),
-                  ],
-                  ),
-                  SizedBox(height: 20),
-                ],
-                //add 버튼, X버튼
-                Row(mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(""),
-                     SizedBox(width: 10),
-                FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      isExpanded = !isExpanded;
-                    });
-                  },
-                  backgroundColor: isExpanded? Color(0xffF7C7C7C): Color(0xffF1E977),
-                  child: isExpanded
-                  ? Image.asset('assets/images/cancel.png')
-                  : Image.asset('assets/images/add.png'),
-                  shape: CircleBorder(),
-                ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    
-    );
-    
   }
-  }
+}
