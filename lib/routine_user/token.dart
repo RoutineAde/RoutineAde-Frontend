f4aa5884db856c3236f8108cb4356adc158f7a84
gaeunpark7 <hen77360@gmail.com>
2024-08-21 14:51:26 +0900

    수정

--- lib/routine_user/token.dart
+++ lib/routine_user/token.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 String token =
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjEwMzkzMDEsImV4cCI6MTczNjU5MTMwMSwidXNlcklkIjoyfQ.XLthojYmD3dA4TSeXv_JY7DYIjoaMRHB7OLx9-l2rvw';
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjQyMTkwNjEsImV4cCI6MTczOTc3MTA2MSwidXNlcklkIjoxfQ.Sg-0YxfAoKNaUesXlt6myQhWt-kLOXxtwgeQs1qqt0k';
 
 // Future<void> saveToken(String token) async {
 //   SharedPreferences prefs = await SharedPreferences.getInstance();
