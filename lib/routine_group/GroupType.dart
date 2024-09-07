db6dd8613dabf6ed82d9d44039afe92dd3f542c0
gaeunpark7 <hen77360@gmail.com>
2024-09-07 15:07:10 +0900

    프로필 불러오기, 개인루틴ui 수정

--- lib/routine_group/GroupType.dart
+++ lib/routine_group/GroupType.dart
@@ -52,7 +52,6 @@ class GroupMember {
       required this.profileImage});
 
   factory GroupMember.fromJson(Map<String, dynamic> json) {
-    //
     return GroupMember(
       userId: json['userId'],
       nickname: json['nickname'],
@@ -107,17 +106,17 @@ class Routine {
   Routine(this.routineTitle, this.repeatDay);
 }
 
+//그룹
 class GroupResponse {
-  //
   final bool isGroupAdmin;
-  final bool isGroupAlarmEnabled;
+  bool isGroupAlarmEnabled;
   final Group groupInfo;
   final List<GroupMember> groupMembers;
   final List<RoutineCategory> groupRoutines;
 
   GroupResponse({
     required this.isGroupAdmin,
-    required this.isGroupAlarmEnabled,
+    this.isGroupAlarmEnabled = false,
     required this.groupInfo,
     required this.groupMembers,
     required this.groupRoutines,
