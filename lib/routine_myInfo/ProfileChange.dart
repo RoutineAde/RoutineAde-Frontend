357f028435c0d7688a8e3cc524cce340d711ae70
gaeunpark7 <hen77360@gmail.com>
2024-09-24 18:13:41 +0900

    사용자 프로필 수정

--- lib/routine_myInfo/ProfileChange.dart
+++ lib/routine_myInfo/ProfileChange.dart
@@ -126,7 +126,7 @@ class _ProfileChangeState extends State<ProfileChange> {
         final mimeTypeData = lookupMimeType(_imageFile!.path)?.split('/');
         if (mimeTypeData != null && mimeTypeData.length == 2) {
           var multipartFile = await http.MultipartFile.fromPath(
-            'profileImage',
+            'image',
             _imageFile!.path,
             contentType: MediaType(mimeTypeData[0], mimeTypeData[1]),
           );
@@ -145,7 +145,7 @@ class _ProfileChangeState extends State<ProfileChange> {
         if (responseBody.isNotEmpty) {
           try {
             final responseData =
-            jsonDecode(utf8.decode(responseBody.codeUnits)); // UTF-8 디코딩
+                jsonDecode(utf8.decode(responseBody.codeUnits)); // UTF-8 디코딩
 
             if (responseData.containsKey('profileImage')) {
               final newProfileImageUrl = responseData['profileImage'];
@@ -213,11 +213,11 @@ class _ProfileChangeState extends State<ProfileChange> {
                         backgroundImage: _imageFile != null
                             ? FileImage(_imageFile!)
                             : _imageUrl != null
-                            ? NetworkImage(
-                            _imageUrl!) // Google Photos 이미지 사용
-                            : const AssetImage(
-                            'assets/images/default_profile.png')
-                        as ImageProvider,
+                                ? NetworkImage(
+                                    _imageUrl!) // Google Photos 이미지 사용
+                                : const AssetImage(
+                                        'assets/images/default_profile.png')
+                                    as ImageProvider,
                       ),
                       Positioned(
                         bottom: 0,
