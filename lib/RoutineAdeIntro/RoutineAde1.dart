6487e83f6afdc8ef4ce00d7561d59b8aeebfdf2f
gaeunpark7 <hen77360@gmail.com>
2024-10-06 17:12:57 +0900

    파이어베이스 연동

--- lib/RoutineAdeIntro/RoutineAde1.dart
+++ lib/RoutineAdeIntro/RoutineAde1.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'WebViewPage.dart';
@@ -20,7 +19,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//webview에서 같이 파이어베이스 토큰을 받도록 수정.
 
 class RoutineAde1 extends StatefulWidget {
   const RoutineAde1({super.key});
@@ -34,43 +32,6 @@ class _RoutineAde1State extends State<RoutineAde1> {
   @override
   void initState() {
     super.initState();
-    _getFirebaseToken(); // 앱 실행 시 FCM 토큰을 가져옴
-  }
-
-  // Firebase 토큰을 가져오는 메서드
-  Future<void> _getFirebaseToken() async {
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-    String? firebaseToken = await messaging.getToken(); // Firebase 토큰 가져오기
-    setState(() {
-      _firebaseToken = firebaseToken;
-    });
-    print('FCM Token: $_firebaseToken');
-  }
-
-  // 서버로 Firebase 토큰을 보내는 메서드
-  Future<void> _sendTokenToServer(String userId, String firebaseToken) async {
-    const String apiUrl = '';
-
-    try {
-      final response = await http.post(
-        Uri.parse('$apiUrl/$userId/token'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token', // 인증 토큰
-        },
-        body: jsonEncode({
-          'token': firebaseToken,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        print('토큰이 성공적으로 서버에 전송되었습니다.');
-      } else {
-        print('서버 응답 에러: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('토큰 전송 실패: $e');
-    }
   }
 
   @override
--- lib/RoutineAdeIntro/WebViewPage.dart
+++ lib/RoutineAdeIntro/WebViewPage.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:routine_ade/RoutineAdeIntro/ProfileSetting.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../routine_user/token.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key});
@@ -12,21 +15,60 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   late WebViewController _controller;
+  String? _firebaseToken;
 
   @override
   void initState() {
     super.initState();
     // 웹뷰 초기화 전에 SharedPreferences에서 토큰을 불러옴.
     _loadToken();
+    _getFirebaseToken(); // 앱 실행 시 FCM 토큰을 가져옴
   }
 
-  // 토큰을 불러와서 출력 (필요시 다른 처리)
+  // 토큰을 불러와서 출력
   Future<void> _loadToken() async {
     String? token = await TokenManager.getToken();
     if (token != null) {
       print('저장된 토큰: $token');
     } else {
       print('토큰이 없습니다.');
+    }
+  }
+
+  // Firebase 토큰 가져오는 메서드
+  Future<void> _getFirebaseToken() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    String? firebaseToken = await messaging.getToken(); // Firebase 토큰 가져오기
+    setState(() {
+      _firebaseToken = firebaseToken;
+    });
+    print('FCM Token: $_firebaseToken');
+  }
+
+  // 서버로 Firebase 토큰을 보내는 메서드
+  Future<void> _sendTokenToServer(String userId, String firebaseToken) async {
+    final String apiUrl = 'http://15.164.88.94/users/$userId/token';
+    String? token = await TokenManager.getToken(); // OAuth 토큰을 가져옴
+
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token', // OAuth 토큰을 추가
+        },
+        body: jsonEncode({
+          'token': firebaseToken,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('토큰이 성공적으로 서버에 전송되었습니다.');
+      } else {
+        print('서버 응답 에러: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('토큰 전송 실패: $e');
     }
   }
 
@@ -38,7 +80,7 @@ class _WebViewPageState extends State<WebViewPage> {
       ),
       body: WebView(
         initialUrl:
-        'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=25a0f887ecba2fdb77884c01ca0325b0&redirect_uri=http://15.164.88.94/users/login/kakao',
+            'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=25a0f887ecba2fdb77884c01ca0325b0&redirect_uri=http://15.164.88.94/users/login/kakao',
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (WebViewController webViewController) {
           _controller = webViewController;
